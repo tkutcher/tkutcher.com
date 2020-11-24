ddde906454bc65fe0c9b48e78e2ff84a6a685ff7
Tim Kutcher <tkutcher@dicorp.com>
2020-11-23 22:05:07 -0500

    SEO updates cause why not (#8)

---
+++ src/app/general/services/seo/seo.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+interface IPage {
+  title: string;
+  description: string;
+  keywords?: string;
+}
+
+const baseKeywords = `
+  Software, 
+  Engineer,
+  Software Engineer, 
+  Baltimore, 
+  App, 
+  Developer, 
+  Angular, 
+  Python,
+  Personal Website`;
+
+const defaultIPage = {
+  title: 'tkutcher.com - Tim Kutcher\'s Personal Website | Software Engineer from Maryland',
+  description: `
+    This is the personal website for Tim Kutcher. I am a software engineer from 
+    Maryland who specializes in full-stack web development (primarily in Python and Angular). 
+    I am also a high school baseball & volleyball coach, and former college athlete.
+    My passions are sports, leadership, friends and family, beautiful code, and beautiful 
+    code that writes beautiful code.
+  `,
+  keywords: baseKeywords
+};
+
+const pageRoutes: Record<string, IPage> = {
+  '/': defaultIPage,
+  '/contact': {
+    title: 'tkutcher.com | Contact Me',
+    description: `
+      Contact me on social media, through GitHub or GitLab, or via an email to
+      discuss any ideas for something I might be able to help build.
+    `,
+    keywords: baseKeywords + ', Contact,'
+  },
+  '/projects': {
+    title: 'tkutcher.com | Projects',
+    description: `
+      Learn about some of the software projects I have worked on and see some of my
+      work.
+    `,
+    keywords: baseKeywords + ', Contact,'
+  },
+  '/bio': {
+    title: 'tkutcher.com | Bio',
+    description: `
+      A history of some of the things I've done.
+    `,
+    keywords: baseKeywords + ', Biography,'
+  }
+};
+
+
+const getIPage = (url) => {
+  let p = url.split('#')[0].split('?')[0];
+  let pathArray = p.split('/');
+
+  while (pathArray.length > 0) {
+    p = pathArray.join('/');
+    p = p == '' ? '/' : p;
+    if (pageRoutes.hasOwnProperty(p)) {
+      return pageRoutes[p]
+    }
+    pathArray = pathArray.slice(0, -1);
+  }
+  return defaultIPage;
+};
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeoService {
+
+  constructor(
+    private title: Title,
+    private meta: Meta) { }
+
+  updateTitleForNewRoute(url) {
+    const pg = getIPage(url);
+    this.title.setTitle(pg.title);
+    this.meta.updateTag({name: 'description', content: pg.description});
+    if (pg.keywords) {
+      this.meta.updateTag({name: 'keywords', content: pg.keywords});
+    }
+  }
+}
--- src/app/nav/nav.service.ts
+++ src/app/nav/nav.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { CmdPromptService } from './cmd-prompt/cmd-prompt.service';
 import { isPlatformBrowser } from '@angular/common';
+import { SeoService } from '../general/services/seo/seo.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,12 @@ export class NavService {
     private router: Router,
     private route: ActivatedRoute,
     private cmdPromptService: CmdPromptService,
+    private seo: SeoService
   ) {
 
     this.router.events.subscribe((e: RouterEvent) => {
       if (e instanceof NavigationEnd) {
+        this.seo.updateTitleForNewRoute(e.url);
         this.cmdPromptService.setPath('~' + (e.url === '/' ? '' : e.url));
         this.cmdPromptService.clearPrompt();
       }
